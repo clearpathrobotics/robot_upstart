19ea156f284217a0e42706d95de299770537c865
Mikael Arguedas <mikael.arguedas@gmail.com>
2020-08-14 23:35:32 +0200

    fix most roslint failures
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>

--- src/robot_upstart/install_script.py
+++ src/robot_upstart/install_script.py
@@ -97,7 +97,7 @@ def main():
 
         found_path = find_in_workspaces(project=pkg, path=pkgpath, first_match_only=True)
         if not found_path:
-            print ("Unable to locate path %s in package %s. Installation aborted." % (pkgpath, pkg))
+            print("Unable to locate path %s in package %s. Installation aborted." % (pkgpath, pkg))
             return 1
 
         if os.path.isfile(found_path[0]):
