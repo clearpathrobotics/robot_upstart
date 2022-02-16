96c219f854204d5ea9e272936967ae191f36ed69
Tony Baltovski <tony.baltovski@gmail.com>
2022-02-16 10:08:00 -0500

    Fixed package.xml and setup.py information.

--- setup.py
+++ setup.py
@@ -2,21 +2,26 @@ import os
 from setuptools import setup
 from glob import glob
 
-package_name = 'robot_upstart'
+PACKAGE_NAME = 'robot_upstart'
 
 setup(
-    name=package_name,
-    packages=[package_name],
+    name=PACKAGE_NAME,
+    version='0.4.2',
+    packages=[PACKAGE_NAME],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('lib', package_name, 'scripts'), glob('scripts/*')),
-        (os.path.join('share', package_name, 'scripts'), glob('scripts/*')),
-        (os.path.join('share', package_name, 'templates'), glob('templates/*')),
+            ['resource/' + PACKAGE_NAME]),
+        ('share/' + PACKAGE_NAME, ['package.xml']),
+        (os.path.join('lib', PACKAGE_NAME, 'scripts'), glob('scripts/*')),
+        (os.path.join('share', PACKAGE_NAME, 'scripts'), glob('scripts/*')),
+        (os.path.join('share', PACKAGE_NAME, 'templates'), glob('templates/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
+    maintainer='Tony Baltovski',
+    maintainer_email='tbaltovski@clearpathrobotics.com',
+    description='The robot_upstart package provides scripts which may be used to install and uninstall Ubuntu Linux upstart jobs which launch groups of roslaunch files.',
+    license='BSD',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
